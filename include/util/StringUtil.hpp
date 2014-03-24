6720576f95a163a4172ae0be940941049029e94f
Sebastian Woehrl <sebastian.woehrl@mytum.de>
2014-03-24 10:32:50 +0100

    Added sql-tests for hyper

--- include/util/StringUtil.hpp
+++ include/util/StringUtil.hpp
@@ -12,5 +12,6 @@ std::vector<std::string> split(std::string work, char delim, int rep=0);
 
 std::string readFile(const std::string& filename);
 
+void replaceAll(std::string& str, const std::string& from, const std::string& to);
 
 }}
